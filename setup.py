053572aedc626722149f28f70957486366f5fc7b
Nicolas Dubois <nicolas.c.dubois@gmail.com>
2012-11-01 22:04:54 +0100

    Fixed URL + import version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-basic-models-behaviors',
-    version='0.2.0',
+    version=__import__('basic_models_behaviors').__version__,
     description='',
     author='Nicolas Dubois',
     author_email='nicolas.c.dubois@gmail.com',
-    url='http://hg.nicolasdubois.com/django-basic-models-behaviors',
+    url='https://github.com/duboisnicolas/django-basic-models-behaviors',
     keywords = "django",
     packages=['basic_models_behaviors'],
     include_package_data=True,
