494856cc924ee4187b6ec0a6627a935ae7cd97bb
Nicolas Dubois <nicolas.dubois@oscaro.com>
2015-07-01 20:35:57 +0200

    Add python versions to setup.py and switch status from Stable to Beta
    I planned to manage python3.3 and python3.4.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description=read('README.rst'),
     author='Nicolas Dubois',
     author_email='nicolas.c.dubois@gmail.com',
-    url='https://github.com/duboisnicolas/django-basic-models-behaviors',
+    url='https://github.com/wo0dyn/django-basic-models-behaviors',
     keywords="django",
     packages=['basic_models_behaviors'],
     include_package_data=True,
@@ -24,12 +24,15 @@ setup(
     license='MIT License',
     platforms=['any'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ]
 )
